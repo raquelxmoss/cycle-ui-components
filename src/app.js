ee1e2cbbfb1cfd46189dfe54719f50276b7739f5
Raquel Moss <raquelxmoss@gmail.com>
2016-04-23 17:57:01 +0200

    Allow notifications to be dismissed

--- src/app.js
+++ src/app.js
@@ -12,12 +12,20 @@ function toggleNotification (state) {
 }
 
 export default function App ({DOM}) {
+  const props$ = Observable.of({
+    message: 'This is a notification',
+    type: 'danger'
+  })
+
+  const notification = Notification({props: props$, DOM})
+
   const click$ = DOM
     .select('.trigger-notification')
     .events('click')
 
   const action$ = Observable.merge(
-    click$.map(_ => toggleNotification)
+    click$.map(_ => toggleNotification),
+    notification.dismiss.map(_ => toggleNotification)
   )
 
   const initialState = {
@@ -27,13 +35,6 @@ export default function App ({DOM}) {
   const state$ = action$
     .scan((state, action) => action(state), initialState)
     .startWith(initialState)
-
-  const props$ = Observable.of({
-    message: 'hello',
-    type: 'danger'
-  })
-
-  const notification = Notification({props: props$})
 
   return {
     DOM: state$.map(state =>
--- src/notification.js
+++ src/notification.js
@@ -2,15 +2,21 @@ import { div, button, p } from '@cycle/dom'
 import { Observable } from 'rx'
 
 export default function Notification(sources) {
+  const dismiss$ = sources.DOM
+    .select('.dismiss')
+    .events('click')
+    .map(_ => true)
+
   const vtree$ = sources.props.map(props =>
-    div('.notification', [
+    div('.notification', { className: props.type }, [
       p('.message', props.message),
-      p(props.type)
+      button('.dismiss', 'X')
     ])
   )
 
   return {
-    DOM: vtree$
+    DOM: vtree$,
+    dismiss: dismiss$
   }
 }
 
